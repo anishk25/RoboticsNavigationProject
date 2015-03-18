7feea90eea0189b6e2eb5cfbf60a46cddd6592b6
Anish Khattar <anish_khattar25@anishs-mbp.home>
2015-03-18 19:05:09 -0400

    added tess two test project

---
+++ Android/TestTwoTest/app/src/main/java/com/robot/tesstwotest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.robot.tesstwotest;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView tvInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvInfo = (TextView)findViewById(R.id.tvInfo);
+        String data_path = "/storage/sdcard0/";
+        File f = new File(data_path + "/TessTwoOcrLang/" + "eng.traineddata");
+
+        tvInfo.setText(f.getTotalSpace()+"");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Android/TestTwoTest/libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/anish_khattar25/Downloads/tess_two/tess-two/bin/AndroidManifest.xml : \
+/Users/anish_khattar25/Downloads/tess_two/tess-two/AndroidManifest.xml \
