f362e957b7cde4ef20d5450a0f6d0a797150cae1
Anish Khattar <anish_khattar25@Anishs-MacBook-Pro.local>
2015-04-14 12:04:40 -0400

    started simple color detection robot comm app

---
+++ Android/RobotCommApp/app/src/main/java/com/robot/processor/app/ColorBlobDetector.java
@@ -0,0 +1,189 @@
+package com.robot.processor.app;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by anish_khattar25 on 4/14/15.
+ */
+
+public class ColorBlobDetector {
+    // Lower and Upper bounds for range checking in HSV color space
+    private Scalar mLowerBound = new Scalar(0);
+    private Scalar mUpperBound = new Scalar(0);
+    // Minimum contour area in percent for contours filtering
+    private static double mMinContourArea = 3000;
+    // Color radius for range checking in HSV color space
+    private Scalar mColorRadius = new Scalar(25,50,50,0);
+    private List<MatOfPoint> mContours = new ArrayList<MatOfPoint>();
+    private static String TAG = ColorBlobDetector.class.getCanonicalName();
+    private int foundSignCount = 0;
+    private int numSignsToSearch;
+
+
+    public static  enum ColorState{
+        SEARCH_FIRST_STATE(-1),RED_STATE(0),YELLOW_STATE(1),SEND_STOP(25),DONE_SEARCHING(50),;
+
+        private final int value;
+        private ColorState(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+
+    };
+
+    private ColorState currColorState = ColorState.SEARCH_FIRST_STATE;
+    private static final Scalar  RED_COLOR = new Scalar(177, 10, 14,255);
+    private static final Scalar YELLOW_COLOR = new Scalar(170, 170, 40, 255);
+
+    private static final Scalar [] COLORS_TO_SEARCH = {RED_COLOR,YELLOW_COLOR};
+    private static final ColorState [] COLOR_STATES = {ColorState.RED_STATE,ColorState.YELLOW_STATE};
+
+
+
+    // Cache
+    Mat mPyrDownMat = new Mat();
+    Mat mHsvMat = new Mat();
+    Mat mMask = new Mat();
+    Mat mDilatedMask = new Mat();
+    Mat mHierarchy = new Mat();
+
+
+
+
+    public ColorBlobDetector(int numSignsToSearch){
+        this.numSignsToSearch = numSignsToSearch;
+    }
+    public void setColorRadius(Scalar radius) {
+        mColorRadius = radius;
+    }
+
+    private void setHsvColor(Scalar hsvColor) {
+        double minH = (hsvColor.val[0] >= mColorRadius.val[0]) ? hsvColor.val[0]-mColorRadius.val[0] : 0;
+        double maxH = (hsvColor.val[0]+mColorRadius.val[0] <= 255) ? hsvColor.val[0]+mColorRadius.val[0] : 255;
+
+        mLowerBound.val[0] = minH;
+        mUpperBound.val[0] = maxH;
+
+        mLowerBound.val[1] = hsvColor.val[1] - mColorRadius.val[1];
+        mUpperBound.val[1] = hsvColor.val[1] + mColorRadius.val[1];
+
+        mLowerBound.val[2] = hsvColor.val[2] - mColorRadius.val[2];
+        mUpperBound.val[2] = hsvColor.val[2] + mColorRadius.val[2];
+
+        mLowerBound.val[3] = 0;
+        mUpperBound.val[3] = 255;
+    }
+
+    public void setMinContourArea(double area) {
+        mMinContourArea = area;
+    }
+
+    private void processColorInImage(Mat rgbaImage) {
+        Imgproc.pyrDown(rgbaImage, mPyrDownMat);
+        Imgproc.pyrDown(mPyrDownMat, mPyrDownMat);
+
+        Imgproc.cvtColor(mPyrDownMat, mHsvMat, Imgproc.COLOR_RGB2HSV_FULL);
+
+        Core.inRange(mHsvMat, mLowerBound, mUpperBound, mMask);
+        Imgproc.dilate(mMask, mDilatedMask, new Mat());
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+        Imgproc.findContours(mDilatedMask, contours, mHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // Find max contour area
+        /*double maxArea = 0;
+        Iterator<MatOfPoint> each = contours.iterator();
+        while (each.hasNext()) {
+            MatOfPoint wrapper = each.next();
+            double area = Imgproc.contourArea(wrapper);
+            if (area > maxArea)
+                maxArea = area;
+        }*/
+
+        // Filter contours by area and resize to fit the original image size
+        Iterator<MatOfPoint> each = contours.iterator();
+
+        each = contours.iterator();
+        mContours.clear();
+        while (each.hasNext()) {
+            MatOfPoint contour = each.next();
+            double contourArea = Imgproc.contourArea(contour);
+            if (contourArea > mMinContourArea) {
+                Core.multiply(contour, new Scalar(4,4), contour);
+                mContours.add(contour);
+            }
+        }
+    }
+
+    public void searchForColor(Mat rgbaImage){
+        List<MatOfPoint> contours;
+        switch(currColorState){
+            case SEARCH_FIRST_STATE:
+                for(int i = 0; i < COLORS_TO_SEARCH.length; i++){
+                    setHsvColor(convertScalarRgba2Hsv(COLORS_TO_SEARCH[i]));
+                    processColorInImage(rgbaImage);
+                    if(mContours.size() > 0){
+                        currColorState = COLOR_STATES[i];
+                        foundSignCount = 1;
+                        break;
+                    }
+                }
+                break;
+            case RED_STATE:
+                setHsvColor(convertScalarRgba2Hsv(RED_COLOR));
+                processColorInImage(rgbaImage);
+                foundSignCount++;
+                if(mContours.size() > 0){
+                    currColorState = COLOR_STATES[(currColorState.getValue() + 1) % COLOR_STATES.length];
+                }
+                break;
+            case YELLOW_STATE:
+                setHsvColor(convertScalarRgba2Hsv(YELLOW_COLOR));
+                processColorInImage(rgbaImage);
+                foundSignCount++;
+                if(mContours.size() > 0){
+                    currColorState = COLOR_STATES[(currColorState.getValue() + 1) % COLOR_STATES.length];
+                }
+                break;
+
+            case DONE_SEARCHING:
+                break;
+        }
+        if(foundSignCount >= numSignsToSearch){
+            currColorState = ColorState.DONE_SEARCHING;
+        }
+    }
+
+    public ColorState getCurrColorState(){
+        return currColorState;
+    }
+
+    public void resetStateMachine(){
+        currColorState = ColorState.SEARCH_FIRST_STATE;
+    }
+
+    private Scalar convertScalarRgba2Hsv(Scalar rgbaColor){
+        Mat pointMatHsv = new Mat();
+        Mat pointMatRgba = new Mat(1,1, CvType.CV_8UC4,rgbaColor);
+        Imgproc.cvtColor(pointMatRgba,pointMatHsv,Imgproc.COLOR_RGB2HSV_FULL,3);
+        return new Scalar(pointMatHsv.get(0,0));
+    }
+
+    public List<MatOfPoint> getContours() {
+        return mContours;
+    }
+}
---
+++ Android/RobotCommApp/app/src/main/java/com/robot/processor/app/MainActivity.java
@@ -0,0 +1,71 @@
+package com.robot.processor.app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.robot.constants.Constants;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private Button bStartCamera;
+    private EditText etNumSigns;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void initUI(){
+        bStartCamera = (Button)findViewById(R.id.bStartCamera);
+        etNumSigns = (EditText)findViewById(R.id.etNumSigns);
+        bStartCamera.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bStartCamera:
+                Intent intent = new Intent(this,ColorBlobDetectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt(Constants.NUM_SIGNS_BUNDLE_KEY,Integer.parseInt(etNumSigns.getText().toString()));
+                intent.putExtras(bundle);
+                break;
+        }
+    }
+}
