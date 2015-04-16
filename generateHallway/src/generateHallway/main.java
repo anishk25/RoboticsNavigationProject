4576d3e72e616e006f401c09230e582287945e93
ryankuo2 <ryankuo2@gmail.com>
2015-04-16 13:23:40 -0400

    Revert "Revert "Added two more hallways""
    This reverts commit c9f4159f1ecee23ccde138e70347e16d91822a50.

--- generateHallway/src/generateHallway/main.java
+++ generateHallway/src/generateHallway/main.java
@@ -7,49 +7,86 @@ import java.util.TreeMap;
 
 public class main {
 	public static void main(String [] args){
-		//TreeMap<Double,Integer>distances = new TreeMap<Double,Integer>();
+		
 		ArrayList<Double>distances = new ArrayList<Double>();
 		ArrayList<Integer>roomNums = new ArrayList<Integer>();
-		ArrayList<Integer>hallway = new ArrayList<Integer>();
-		distances.add(new Double(8.5));
-		distances.add(new Double(13));
-		distances.add(new Double(8.5));
-		distances.add(new Double(13));
-		distances.add(new Double(8));
-		distances.add(new Double(14));
-		distances.add(new Double(10));
-		distances.add(new Double(10));
-		distances.add(new Double(6));
-		distances.add(new Double(17));
-		distances.add(new Double(10));
-		distances.add(new Double(12));
-		distances.add(new Double(10));
-		roomNums.add(new Integer(1409));
-		roomNums.add(new Integer(1411));
-		roomNums.add(new Integer(1415));
-		roomNums.add(new Integer(1415));
-		roomNums.add(new Integer(1417));
-		roomNums.add(new Integer(1417));
-		roomNums.add(new Integer(1421));
-		roomNums.add(new Integer(1423));
-		roomNums.add(new Integer(1427));
-		roomNums.add(new Integer(1427));
-		roomNums.add(new Integer(1431));
-		roomNums.add(new Integer(1431));
-		roomNums.add(new Integer(1433));
+		ArrayList<Integer>blankenshipHallway = new ArrayList<Integer>();
+		ArrayList<Integer>rennerHallway = new ArrayList<Integer>();
+		ArrayList<Integer>sunLabHallway = new ArrayList<Integer>();
+		// Generate Blankenship Hallway
+		double [] blankenshipDistances = {8.5,13,8.5,13,8,14,10,10,6,17,10,12,10};
+		int [] blankenshipRooms = {1409, 1411, 1415,1415,1417,1417,1421,1423,1427,1427,1431,1431,1433};
+		for(int i=0;i<blankenshipDistances.length;++i){
+			distances.add(blankenshipDistances[i]);
+		}
+		for(int i=0;i<blankenshipRooms.length;++i){
+			roomNums.add(blankenshipRooms[i]);
+		}
 		
 		for(int j=0;j<distances.size();++j) {
 			  Double distance = distances.get(j);
 			  Integer room = roomNums.get(j);
 
 			for(double i=0.5;i<distance;i+=0.5){
-				hallway.add(0);
+				blankenshipHallway.add(0);
 			}
-			hallway.add(room);
+			blankenshipHallway.add(room);
 		}
 		
-		for(int i=0;i<hallway.size();++i){
-			System.out.print(hallway.get(i)+" ");
+		// Generate Renner Hallway
+		distances.clear();
+		roomNums.clear();
+		double [] rennerDistances = {10.5,11,9,12,9, 13,11,9,11,13,9,13,9,9,9,11,7,27,8,33.5};
+		int [] rennerRooms = {1315,1317,1321,1321,1323,1323,1325,1327,1329,1329,1333,1333,1337,1337,1339,1498,1498,1405,1405,9000};
+		for(int i=0;i<rennerDistances.length;++i){
+			distances.add(rennerDistances[i]);
+		}
+		for(int i=0;i<rennerRooms.length;++i){
+			roomNums.add(rennerRooms[i]);
+		}
+		
+		for(int j=0;j<distances.size();++j) {
+			  Double distance = distances.get(j);
+			  Integer room = roomNums.get(j);
+
+			for(double i=0.5;i<distance;i+=0.5){
+				rennerHallway.add(0);
+			}
+			rennerHallway.add(room);
+		}
+		
+		// Generate SunLab Hallway
+		distances.clear();
+		roomNums.clear();
+		double [] sunLabDistances = {38.5,5,24,5,6,5,28,6,21,5,12};
+		int [] sunLabRooms = {1458,1458,1454,1454,1450,1450,1446,1446,1442,1442,5000};
+		for(int i=0;i<sunLabDistances.length;++i){
+			distances.add(sunLabDistances[i]);
+		}
+		for(int i=0;i<sunLabRooms.length;++i){
+			roomNums.add(sunLabRooms[i]);
+		}
+		
+		for(int j=0;j<distances.size();++j) {
+			  Double distance = distances.get(j);
+			  Integer room = roomNums.get(j);
+
+			for(double i=0.5;i<distance;i+=0.5){
+				sunLabHallway.add(0);
+			}
+			sunLabHallway.add(room);
+		}
+		
+		for(int i=0;i<blankenshipHallway.size();++i){
+			System.out.print(blankenshipHallway.get(i)+" ");
+		}
+		System.out.println();
+		for(int i=0;i<rennerHallway.size();++i){
+			System.out.print(rennerHallway.get(i)+" ");
+		}
+		System.out.println();
+		for(int i=0;i<sunLabHallway.size();++i){
+			System.out.print(sunLabHallway.get(i)+" ");
 		}
 	}
 }
