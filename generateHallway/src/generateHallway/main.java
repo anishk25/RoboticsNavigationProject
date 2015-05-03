5c3f957f06c75e4ea8ae298feccd47f74a4eb2b1
ryankuo2 <ryankuo2@gmail.com>
2015-05-03 15:24:29 -0400

    Generate Hallway legit
    Generate Hallway legit

--- generateHallway/src/generateHallway/main.java
+++ generateHallway/src/generateHallway/main.java
@@ -8,78 +8,87 @@ import java.util.TreeMap;
 public class main {
 	public static void main(String [] args){
 		
-		ArrayList<Double>distances = new ArrayList<Double>();
-		ArrayList<Integer>roomNums = new ArrayList<Integer>();
+		int startRoomNum = 1431;
+		int endRoomNum = 1411;
+		
+		ArrayList<Double>distancesB = new ArrayList<Double>();
+		ArrayList<Integer>roomNumsB = new ArrayList<Integer>();
+		ArrayList<Double>distancesR = new ArrayList<Double>();
+		ArrayList<Integer>roomNumsR = new ArrayList<Integer>();
+		ArrayList<Double>distancesS = new ArrayList<Double>();
+		ArrayList<Integer>roomNumsS = new ArrayList<Integer>();
+		ArrayList<Double>curRoomsDistances = new ArrayList<Double>();
 		ArrayList<Integer>blankenshipHallway = new ArrayList<Integer>();
 		ArrayList<Integer>rennerHallway = new ArrayList<Integer>();
 		ArrayList<Integer>sunLabHallway = new ArrayList<Integer>();
 		ArrayList<Integer> curHallway;
+		ArrayList<String> hallProgression = new ArrayList<String>();
+		ArrayList<String> turnProgression = new ArrayList<String>();
+		String colorPaperToLook = null;
+		String direction = "";
 		double curPos = 0; //represents the current position along the current hallway
 		// Generate Blankenship Hallway
 		//Added 5 at end of array to signify end of hallway, using 9001 to signify this
-		double [] blankenshipDistances = {6,13,8.5,13,8,14,10,10,6,17,10,12,10,5};
+		double [] blankenshipDistances = {6,13,9,13,8,14,10,10,6,17,10,12,10,5};
 		int [] blankenshipRooms = {1409, 1411, 1415,1415,1417,1417,1421,1423,1427,1427,1431,1431,1433,9001};
+		double [] rennerDistances = {8,11,9,12,9, 13,11,9,11,13,9,13,9,9,9,11,7,8,19,8,31};
+		int [] rennerRooms = {1315,1317,1321,1321,1323,1323,1325,1327,1329,1329,1333,1333,1337,1337,1339,1498,1498,5000,1405,1405,5001};
+		double [] sunLabDistances = {36,5,24,5,6,5,28,6,21,5,12};
+		int [] sunLabRooms = {1458,1458,1454,1454,1450,1450,1446,1446,1442,1442,9000};
+		
 		for(int i=0;i<blankenshipDistances.length;++i){
-			distances.add(blankenshipDistances[i]);
+			distancesB.add(blankenshipDistances[i]);
 		}
 		for(int i=0;i<blankenshipRooms.length;++i){
-			roomNums.add(blankenshipRooms[i]);
+			roomNumsB.add(blankenshipRooms[i]);
 		}
 		
-		for(int j=0;j<distances.size();++j) {
-			  Double distance = distances.get(j);
-			  Integer room = roomNums.get(j);
+		for(int j=0;j<distancesB.size();++j) {
+			  Double distance = distancesB.get(j);
+			  Integer room = roomNumsB.get(j);
 
-			for(double i=0.5;i<distance;i+=0.5){
+			for(double i=0;i<distance;i+=1){
 				blankenshipHallway.add(0);
 			}
 			blankenshipHallway.add(room);
 		}
-		
+		blankenshipHallway.set(0, 4000);
 		// Generate Renner Hallway
-		distances.clear();
-		roomNums.clear();
-		double [] rennerDistances = {8,11,9,12,9, 13,11,9,11,13,9,13,9,9,9,11,7,8,19,8,31};
-		int [] rennerRooms = {1315,1317,1321,1321,1323,1323,1325,1327,1329,1329,1333,1333,1337,1337,1339,1498,1498,5000,1405,1405,5001};
 		for(int i=0;i<rennerDistances.length;++i){
-			distances.add(rennerDistances[i]);
+			distancesR.add(rennerDistances[i]);
 		}
 		for(int i=0;i<rennerRooms.length;++i){
-			roomNums.add(rennerRooms[i]);
+			roomNumsR.add(rennerRooms[i]);
 		}
 		
-		for(int j=0;j<distances.size();++j) {
-			  Double distance = distances.get(j);
-			  Integer room = roomNums.get(j);
+		for(int j=0;j<distancesR.size();++j) {
+			  Double distance = distancesR.get(j);
+			  Integer room = roomNumsR.get(j);
 
-			for(double i=0.5;i<distance;i+=0.5){
+			for(double i=0;i<distance;i+=1){
 				rennerHallway.add(0);
 			}
 			rennerHallway.add(room);
 		}
 		
 		// Generate SunLab Hallway
-		distances.clear();
-		roomNums.clear();
-		double [] sunLabDistances = {36,5,24,5,6,5,28,6,21,5,12};
-		int [] sunLabRooms = {1458,1458,1454,1454,1450,1450,1446,1446,1442,1442,9000};
 		for(int i=0;i<sunLabDistances.length;++i){
-			distances.add(sunLabDistances[i]);
+			distancesS.add(sunLabDistances[i]);
 		}
 		for(int i=0;i<sunLabRooms.length;++i){
-			roomNums.add(sunLabRooms[i]);
+			roomNumsS.add(sunLabRooms[i]);
 		}
 		
-		for(int j=0;j<distances.size();++j) {
-			  Double distance = distances.get(j);
-			  Integer room = roomNums.get(j);
+		for(int j=0;j<distancesS.size();++j) {
+			  Double distance = distancesS.get(j);
+			  Integer room = roomNumsS.get(j);
 
-			for(double i=0.5;i<distance;i+=0.5){
+			for(double i=0;i<distance;i+=1){
 				sunLabHallway.add(0);
 			}
 			sunLabHallway.add(room);
 		}
-		
+		sunLabHallway.set(0, 4001);
 		// print out hallway arrays
 		/*for(int i=0;i<blankenshipHallway.size();++i){
 			System.out.print(blankenshipHallway.get(i)+" ");
@@ -93,71 +102,143 @@ public class main {
 			System.out.print(sunLabHallway.get(i)+" ");
 		}*/
 		
+		int roomNumsIndex = 0;
+		if(blankenshipHallway.contains(startRoomNum)){
+			roomNumsIndex =  roomNumsB.indexOf(startRoomNum);
+		}
+		else if(rennerHallway.contains(startRoomNum)){
+			roomNumsIndex =  roomNumsR.indexOf(startRoomNum);
+		}
+		else if(sunLabHallway.contains(startRoomNum)){
+			roomNumsIndex =  roomNumsS.indexOf(startRoomNum);
+		}
 		int turnAt;
-		// Will be populated later (HTTP Request?)
-		int roomNumToFind = 1417;
-		if(blankenshipHallway.contains(roomNumToFind))
-			turnAt = 5001;
-		if(sunLabHallway.contains(roomNumToFind))
-			turnAt = 5000;
+		
+		if(blankenshipHallway.contains(startRoomNum) && blankenshipHallway.contains(endRoomNum)){
+			hallProgression.add("b");
+		}
+		else if(blankenshipHallway.contains(startRoomNum) && sunLabHallway.contains(endRoomNum)){
+			hallProgression.add("brs");
+			turnProgression.add("Left");
+			turnProgression.add("Left");
+		}
+		else if(blankenshipHallway.contains(startRoomNum) && rennerHallway.contains(endRoomNum)){
+			hallProgression.add("br");
+			turnProgression.add("Left");
+		}
+		else if(sunLabHallway.contains(startRoomNum) && blankenshipHallway.contains(endRoomNum)){
+			hallProgression.add("srb");
+			turnProgression.add("Right");
+			turnProgression.add("Right");
+		}
+		else if(sunLabHallway.contains(startRoomNum) && sunLabHallway.contains(endRoomNum)){
+			hallProgression.add("s");
+		}
+		else if(sunLabHallway.contains(startRoomNum) && rennerHallway.contains(endRoomNum)){
+			hallProgression.add("sr");
+			if(rennerHallway.indexOf(endRoomNum) < rennerHallway.indexOf(5000))
+				turnProgression.add("Left");
+			else
+				turnProgression.add("Right");
+		}
+		else if(rennerHallway.contains(startRoomNum) && blankenshipHallway.contains(endRoomNum)){
+			hallProgression.add("rb");
+			turnProgression.add("Right");
+		}
+		else if(rennerHallway.contains(startRoomNum) && sunLabHallway.contains(endRoomNum)){
+			hallProgression.add("rs");
+			if(rennerHallway.indexOf(endRoomNum) < rennerHallway.indexOf(5000))
+				turnProgression.add("Right");
+			else
+				turnProgression.add("Left");
+		}
+		else if(rennerHallway.contains(startRoomNum) && rennerHallway.contains(endRoomNum)){
+			hallProgression.add("r");
+		}
+
+		if(hallProgression.get(0).equals("b"))
+			curHallway = blankenshipHallway;
+		else if(hallProgression.get(0).equals("s"))
+			curHallway = sunLabHallway;
+		else
+			curHallway = rennerHallway;
 		
 		boolean end=false;
-		//Used to find where robot is on map as well as if it needs to do a map change
-		while(end==false){
-			curHallway = rennerHallway;
-			if(curHallway.get((int)(curPos/0.5)) == 9000){
-				System.out.println("End of Sun lab");
-				end = true;
+		curPos = curHallway.indexOf(startRoomNum);
+		//System.out.println(curPos+" "+roomNumsIndex);
+		while(hallProgression.size() > 0){
+			if(hallProgression.size()>1){
+				if(hallProgression.get(0).equals("b") && hallProgression.get(1).equals("r")){
+					int indexTurn = 4000;
+					direction = "backwards";
+				}
+				else if(hallProgression.get(0).equals("s") && hallProgression.get(1).equals("r")){
+					int indexTurn = 4001; 
+					direction = "backwards";
+				}
+				else if(hallProgression.get(0).equals("r") && hallProgression.get(1).equals("b")){
+					int indexTurn = 5001;
+					direction = "forward";
+				}
+				else if(hallProgression.get(0).equals("r") && hallProgression.get(1).equals("s")){
+					int indexTurn = 5000;
+					if(curHallway.indexOf(curPos) < curHallway.indexOf(5000))
+						direction="forward";
+					else
+						direction="backward";
+				}
+					
 			}
-			if(curHallway.get((int)(curPos/0.5)) == 9001){
-				System.out.println("End of Blankenship hallway");
-				end = true;
+			else{
+				if(curPos < curHallway.indexOf(endRoomNum))
+					direction="forward";
+				else
+					direction="backward";
 			}
-			if(curHallway.get((int)(curPos/0.5)) == 5000){
-				//Turn into sun lab hallway
-				curHallway = sunLabHallway;
-				curPos = 0;
-			}
-			if(curHallway.get((int)(curPos/0.5)) == 5001){
-				//Turn into Blankenship hallway
+			
+			/*if(//update encoder by 1)
+				if(direction.equals("forward"))
+					curPos += 1;
+				else
+					curPos -= 1;*/
+			if(hallProgression.get(0).equals("b")){
 				curHallway = blankenshipHallway;
-				curPos = 0;
+				curRoomsDistances = distancesB;
 			}
-			if(curHallway.get((int)(curPos/0.5)) == roomNumToFind){
+			else if(hallProgression.get(0).equals("s")){
+				curHallway = sunLabHallway;
+				curRoomsDistances = distancesS;
+			}
+			else{
+				curHallway = rennerHallway;
+				curRoomsDistances = distancesR;
+			}
+			//System.out.println(curPos);
+			if(true)
+				if(direction.equals("forward")){
+					roomNumsIndex += 1;
+					curPos = -1;
+					//System.out.println(roomNumsIndex);
+					for(int i=0;i<=roomNumsIndex;++i){
+						curPos += curRoomsDistances.get(i);
+						curPos += 1;
+					}
+				}
+				else{
+					roomNumsIndex -= 1;
+					curPos = -1;
+					for(int i=0;i<=roomNumsIndex;++i){
+						curPos += curRoomsDistances.get(i);
+						curPos += 1;
+					}
+				}
+			//System.out.println(curPos+" "+curHallway.get((int)(curPos)));
+			if(curHallway.get((int)(curPos)) == endRoomNum){
 				end = true;
-				System.out.println("Found the room");
+				hallProgression.clear();
+				//System.out.println("Found the room");
 			}
 			//curPos += getHowMuchFurtherRobotHasGone or curPos = exactDistMoved
 		}
-		// Used for me to see and check map
-		/*int numZeros = 0;
-		for(int i=0;i<blankenshipHallway.size();++i){
-			if(blankenshipHallway.get(i)==0)
-				numZeros++;
-			else{
-				System.out.print(numZeros+" "+blankenshipHallway.get(i)+" ");
-				numZeros = 0;
-			}
-		}
-		System.out.println();
-		numZeros = 0;
-		for(int i=0;i<rennerHallway.size();++i){
-			if(rennerHallway.get(i)==0)
-				numZeros++;
-			else{
-				System.out.print(numZeros+" "+rennerHallway.get(i)+" ");
-				numZeros = 0;
-			}
-		}
-		numZeros = 0;
-		System.out.println();
-		for(int i=0;i<sunLabHallway.size();++i){
-			if(sunLabHallway.get(i)==0)
-				numZeros++;
-			else{
-				System.out.print(numZeros+" "+sunLabHallway.get(i)+" ");
-				numZeros = 0;
-			}
-		}*/
 	}
 }
